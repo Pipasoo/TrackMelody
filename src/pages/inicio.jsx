eda96c194bd06c1ea2b341a8473da408aef285e7
Felipe Godoy <80706667+Pipasoo@users.noreply.github.com>
2023-06-27 23:12:00 -0400

    Add files via upload

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -1,32 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+import {
+    FaBars,
+    FaBook,
+    FaSistrix,
+    FaHome
+} from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 import logo from '../componentes/logo.png';
 import { Link } from 'react-router-dom';
-import { HouseDoorFill, Book, Search } from 'react-bootstrap-icons';
 
-function Inicio() {
+const Inicio = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const menuItem = [
+        {
+            path: "/inicio",
+            name: "Inicio",
+            icon: <FaHome />
+        },
+        {
+            path: "/",
+            name: "Buscar",
+            icon: <FaSistrix />
+        },
+        {
+            path: "/biblioteca",
+            name: "Biblioteca",
+            icon: <FaBook />
+        },
+
+
+    ]
     return (
-        <div className="sidebar">
-            <Link to="/">
-                <img className='logo' src={logo} alt="Logo" />
-                <font color="white">TrackMelody</font>
-            </Link>
-            <ul>
-                <li>
-                    <div className="violet-container">
-                        <div><Link to="/inicio"><font color="white"><HouseDoorFill className="me-2" />Inicio</font></Link></div>
-                        <div><Link to="/inicio"><font color="white"><Search className="me-2" />Buscar</font></Link></div>
+        <div className="containerR">
+            <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+                <div style={{ display: isOpen ? "block" : "none" }} className="logo">
+                    <Link to="/">
+                        <img className='logo' src={logo} alt="Logo" />
+                    </Link>
+                </div>
+                <div className="top_section">
+                    <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+                        <FaBars onClick={toggle} />
                     </div>
-                </li>
-                <li>
-                    <div className="violet-container">
-                        <Link to="/"><font color="white"><Book className="me-2" />Biblioteca</font></Link>
-                    </div>
-                </li>
-            </ul>
+                </div>
+                {
+                    menuItem.map((item, index) => (
+                        <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                            <div className="icon">{item.icon}</div>
+                            <div style={{ display: isOpen ? "block" : "none" }} className="link_text">{item.name}</div>
+                        </NavLink>
+                    ))
+                }
+            </div>
+            <main>{children}</main>
         </div>
     );
+
 }
 
 export default Inicio;
